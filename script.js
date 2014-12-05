1296267d88a4b37640333d4baf110791858c25aa
IrshadAM <irshad.mallick@hotmail.com>
2014-12-05 17:53:11 +0300

    Made zen message fade into screen
    Chained up a hide + appendTo + fadeIn.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   var api = "https://api.github.com/zen";
   $.get(api, function(zenMsg){
-    //Logs zenMsg to console just for testing purposes.
     console.log(zenMsg);
-    $(".zen").append('<div id="zen-message">' + zenMsg + '</div>');
+    var zenMsgDiv = '<div id="zen-message">' + zenMsg + '</div>';
+    $(zenMsgDiv).hide().appendTo(".zen").fadeIn("slow", function(){
+      console.log("Fade in successful!");});
+    });
   });
-});
